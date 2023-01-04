f093b6fb92e2621c80fa61b6f002299d14a709c6
Wish Suharitdamrong <peterwisu@gmail.com>
2023-01-04 02:38:01 +0000

    fix one dark

--- peterwisu/packer.lua
+++ peterwisu/packer.lua
@@ -71,9 +71,8 @@ require('packer').startup(function(use)
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
 
-  --use 'navarasu/onedark.nvim' -- Theme inspired by Atom
+  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
 
-  use 'ful1e5/onedark.nvim'
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
